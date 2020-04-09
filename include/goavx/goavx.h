366e143432c7901e183bdc2aca690e030190797e
ben <bin.go2011@yahoo.com>
2020-04-10 00:47:15 +0800

    add a common header file

---
+++ include/goavx/goavx.h
@@ -0,0 +1,14 @@
+#ifndef CV_GOAVX_API_H_
+#define CV_GOAVX_API_H_
+
+#include "av/format.h"
+#include "av/codec.h"
+#include "av/packet.h"
+#include "av/util.h"
+#include "cv/core.h"
+#include "cv/highgui.h"
+#include "cv/imgcodecs.h"
+#include "cv/videoio.h"
+#include "common/utils.h"
+
+#endif
