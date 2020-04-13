7a4b59c2a3041d9d59053293ca0849f826ec19e1
ben <bin.go2011@yahoo.com>
2020-04-14 00:10:57 +0800

    type bool is allowed

--- src/cv/imgproc.cpp
+++ src/cv/imgproc.cpp
@@ -10,7 +10,7 @@ void _cv_pyrdown(MatPtr src, MatPtr dst)
     cv::pyrDown(*src, *dst);
 }
 
-void _cv_canny( MatPtr image, MatPtr edges, double threshold1, double threshold2, int apertureSize, int L2gradient)
+void _cv_canny( MatPtr image, MatPtr edges, double threshold1, double threshold2, int apertureSize, bool L2gradient)
 {
     cv::Canny(*image, *edges, threshold1, threshold2, apertureSize, L2gradient);
 }
