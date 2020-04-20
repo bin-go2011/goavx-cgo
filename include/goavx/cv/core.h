ad6445de0eff3c0ec89be8cb72e5a5b58237d79a
Ben Ji <bin.go2011@yahoo.com>
2020-04-20 11:14:56 +0800

    rows and cols are public mat properties

--- include/goavx/cv/core.h
+++ include/goavx/cv/core.h
@@ -17,8 +17,6 @@ int _cv_version();
 MatPtr _cv_new_mat();
 void _cv_release_mat(MatPtr mat);
 
-int _cv_mat_rows(MatPtr mat);
-int _cv_mat_cols(MatPtr mat);
 int _cv_mat_channels(MatPtr mat);
 
 #ifdef __cplusplus
