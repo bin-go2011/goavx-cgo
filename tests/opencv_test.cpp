247a62bf830bb1ae693920d90f66315e19a41053
Ben Ji <bin.go2011@yahoo.com>
2020-04-22 11:59:47 +0800

    testing open camera

--- tests/opencv_test.cpp
+++ tests/opencv_test.cpp
@@ -4,6 +4,7 @@
 #include <catch.hpp>
 
 #include "cv/core.h"
+#include "cv/videoio.h"
 
 TEST_CASE("basic test", "[opencv]" ) {
     std::cout << std::hex << _cv_version() << std::endl;
@@ -13,4 +14,14 @@ TEST_CASE("mat properties", "[opencv]") {
     MatPtr mat = _cv_new_mat();
 
     printf("rows offset: %d\n", (char*)(&mat->rows) - (char*)(mat));
+}
+
+TEST_CASE("open camera", "[opencv]") {
+	int ret;
+	VideoCapturePtr cap = _cv_new_videocapture();
+	ret= _cv_videocapture_open_device(cap, 0);
+	int width = _cv_videocapture_get(cap, cv::CAP_PROP_FRAME_WIDTH);
+	int height = _cv_videocapture_get(cap, cv::CAP_PROP_FRAME_HEIGHT);
+	int fps = _cv_videocapture_get(cap, cv::CAP_PROP_FPS);
+
 }
